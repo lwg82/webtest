ec80f566fe9c692ae7a0e52b44d5d2b42bffb204
lwg <shuangyan5230@163.com>
2018-03-18 22:12:29 +0800

    完成tomcat配置

---
+++ src/main/java/com/test/pojo/User.java
@@ -0,0 +1,46 @@
+package com.test.pojo;
+
+public class User {
+    public User()
+    {
+        super();
+    }
+
+    public User(String telephoneNumber, String userName)
+    {
+        super();
+        this.telephoneNumber = telephoneNumber;
+        this.userName = userName;
+    }
+
+
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public String getTelephoneNumber() {
+        return telephoneNumber;
+    }
+
+    public void setTelephoneNumber(String telephoneNumber) {
+        this.telephoneNumber = telephoneNumber;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    private Integer userId;
+    private String telephoneNumber;
+    private String userName;
+
+}
